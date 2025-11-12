96249816e135a68b07d01bb4e6de852a8956f5ff
Will Breeden <will@low6.co.uk>
2025-11-12 15:11:37 +0000

    Added Gross Revenue Band to PLN

--- models/pln_arcade/marts/users/pln_arcade__user_stats.sql
+++ models/pln_arcade/marts/users/pln_arcade__user_stats.sql
@@ -17,10 +17,13 @@ user_stats as (
         age_band,
         is_enabled,
         has_consented_pln_marketing,
+        gross_revenue_band,
         count(*) as users,
-        sum(coin_balance) as total_coins
+        sum(coin_balance) as total_coins,
+        sum(total_gross_revenue) as total_gross_revenue,
+        sum(case when total_gross_revenue > 0 then 1 else 0 end) as paying_users
     from users
-    group by 1,2,3,4,5,6,7
+    group by 1,2,3,4,5,6,7,8
 
 )
 
--- models/pln_arcade/marts/users/pln_arcade__users.sql
+++ models/pln_arcade/marts/users/pln_arcade__users.sql
@@ -31,6 +31,16 @@ coin_balances as (
 
 ),
 
+user_purchases as (
+
+    select
+        user_id,
+        sum(price) as total_gross_revenue
+    from purchases
+    group by 1
+
+),
+
 joined as (
 
     select
@@ -44,6 +54,18 @@ joined as (
         users.is_enabled,
         users.has_consented_pln_marketing,
         coalesce(coin_balances.coin_balance,0) as coin_balance,
+        coalesce(user_purchases.total_gross_revenue,0) as total_gross_revenue,
+        case
+            when total_gross_revenue is null then ''
+            when total_gross_revenue <= 0 then ''
+            when total_gross_revenue < 5 then '<£5'
+            when total_gross_revenue < 10 then '£5-9.99'
+            when total_gross_revenue < 25 then '£10-24.99'
+            when total_gross_revenue < 50 then '£25-49.99'
+            when total_gross_revenue < 75 then '£50-74.99'
+            when total_gross_revenue < 100 then '£75-99.99'
+            else '£100+'
+        end as gross_revenue_band,
         users.user_created_date,
         users.user_created_date_et,
         users.user_created_at,
@@ -51,6 +73,8 @@ joined as (
     from users
     left join coin_balances
         on users.user_id = coin_balances.user_id
+    left join user_purchases
+        on users.user_id = user_purchases.user_id
     
 )
 
